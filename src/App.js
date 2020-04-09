e27167f177601fdbc73d66e2d095b8d36c8fd25f
ivandi1980 <ivandi.djoh@gmail.com>
2020-04-09 20:39:30 +0700

    updating App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import youtube from "./api/youtube";
 
 class App extends React.Component {
   render() {
-    return <h3>Selamat Datang di Youtube</h3>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* SEARCH_BAR */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* VIDEO_DETAIL */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* VIDEO_LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
