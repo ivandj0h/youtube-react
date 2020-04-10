4f466073e21064b5d55111178e86a9c7a74bfcde
ivandi1980 <ivandi.djoh@gmail.com>
2020-04-10 21:25:22 +0700

    updated env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,13 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: "AIzaSyDGGzpc6slNJtqzBNfgNxZB2gkghWfvSi8",
-  },
 });
-
-// BASE_URLS : https://www.googleapis.com/youtube/v3
-// API_KEY   : AIzaSyDGGzpc6slNJtqzBNfgNxZB2gkghWfvSi8
-// API_ID    : api-project-569886459615
